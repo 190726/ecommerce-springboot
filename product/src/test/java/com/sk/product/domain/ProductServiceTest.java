ae491245473c825ad1f91a37d65ac2975b9a847f
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-03-18 08:13:19 +0900

    created InmemoryPersistenceAdapter

--- product/src/test/java/com/sk/product/domain/ProductServiceTest.java
+++ product/src/test/java/com/sk/product/domain/ProductServiceTest.java
@@ -1,9 +1,16 @@
 package com.sk.product.domain;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Required;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductServiceTest {
 
@@ -11,18 +18,12 @@ public class ProductServiceTest {
 
     @BeforeEach
     void init() {
-        ProductPersistencePort productPersistencePort = new ProductPersistencePort() {
-            @Override
-            public Product save(Product product) {
-                return product;
-            }
-        };
+        ProductPersistencePort productPersistencePort = new InMemoryPersistenceAdapter();
         productService = new ProductService(productPersistencePort);
     }
 
     @Test
     void registerProduct() {
-
         Product product = new Product();
         productService.register(product);
     }
@@ -32,18 +33,33 @@ public class ProductServiceTest {
         private final ProductPersistencePort productPersistencePort;
 
         public void register(Product product) {
-            productPersistencePort.save(product);
+            final var saved = productPersistencePort.save(product);
+            assertThat(saved.getId()).isEqualTo(1L);
         }
-
-
     }
 
+    @Getter
     private class Product {
 
+        private long id;
+
+        public void id(long id) {
+            this.id = id;
+        }
     }
 
     private interface ProductPersistencePort {
         Product save(Product product);
     }
-}
 
+    private class InMemoryPersistenceAdapter implements ProductPersistencePort {
+        Map<Long, Product> persistenceMap = new HashMap<>();
+        private long sequence = 0L;
+        @Override
+        public Product save(Product product) {
+            persistenceMap.put(++sequence, product);
+            product.id(sequence);
+            return product;
+        }
+    }
+}
