bed74f20595ecfa2f05a35702b3b3efd427ac180
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-03-26 00:41:31 +0900

    failed ProductControllerTest 생성
    ProductRegisterController에 static으로 ProductRegisterRequest DTO 생성

---
+++ product/src/main/java/com/sk/product/adapter/in/ProductRegisterController.java
@@ -0,0 +1,15 @@
+package com.sk.product.adapter.in;
+
+import java.math.BigDecimal;
+
+public class ProductRegisterController {
+
+    public void register(ProductRegisterRequest request) {
+        throw new UnsupportedOperationException("not implement");
+    }
+
+    static class ProductRegisterRequest {
+        public ProductRegisterRequest(String 상품명, BigDecimal one, long l) {
+        }
+    }
+}
--- product/src/test/java/com/sk/product/adapter/in/ProductControllerTest.java
+++ product/src/test/java/com/sk/product/adapter/in/ProductControllerTest.java
@@ -1,5 +1,25 @@
 package com.sk.product.adapter.in;
 
+import com.sk.product.adapter.in.ProductRegisterController.ProductRegisterRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 public class ProductControllerTest {
+
+
+    private ProductRegisterController productController = new ProductRegisterController();
+
+    @Test
+    @DisplayName("ProductControllerTest")
+    void controllerTest() {
+        // given
+        ProductRegisterRequest request = new ProductRegisterRequest("상품명", BigDecimal.ONE, 10L);
+        productController.register(request);
+        // when
+        
+        // then
+    }
 
 }
