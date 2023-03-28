f0bc9bdf99d62f4f0128dcd0ee26ffd9666f9af2
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-03-29 07:42:15 +0900

    ExceptionHandler about ProductNotExistException

--- product/src/main/java/com/sk/product/adapter/in/ProductFetchController.java
+++ product/src/main/java/com/sk/product/adapter/in/ProductFetchController.java
@@ -1,7 +1,12 @@
 package com.sk.product.adapter.in;
 
+import com.sk.product.application.ProductNotExistException;
 import com.sk.product.application.usecase.ProductFetchQuery;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +25,13 @@ public class ProductFetchController {
     }
 
     static record ProductFetchResponse(String name, BigDecimal price, long amount){
+    }
+
+    @ExceptionHandler(ProductNotExistException.class)
+    public ResponseEntity<ProductFetchErrorResponse> productNotException(ProductNotExistException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ProductFetchErrorResponse(404, ex.getMessage()));
+    }
+
+    static record ProductFetchErrorResponse(long id, String message){
     }
 }
